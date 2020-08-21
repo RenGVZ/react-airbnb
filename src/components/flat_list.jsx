7a88696ed38c615a8c7ff212a6035d245b19fb73
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-08-21 23:14:21 +0900

    created all of the components in jsx files

---
+++ src/components/flat_list.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FlatList extends Component {
+  render() {
+    return(
+      <div>Flat list</div>
+    );
+  }
+}
+
+export default FlatList;
---
+++ src/components/marker.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Marker extends Component {
+  render() {
+    return(
+      <div>Marker</div>
+    );
+  }
+}
+
+export default Marker;
